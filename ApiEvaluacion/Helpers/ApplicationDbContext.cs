c5c9cabcd3844e2b272b8a771532751946cbb791
CF0068\Walter Bermudez <walter.martinez04@gmail.com>
2022-04-27 16:26:21 -0600

    Creacion de proyecto base

---
+++ ApiEvaluacion/Helpers/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿
+using ApiEvaluacion.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEvaluacion.Helpers
+{
+    public class ApplicationDbContext : DbContext
+{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
+
+
+
+
+    }
+}
---
+++ ApiEvaluacion/Models/Empleado.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiEvaluacion.Models
+{
+    public class Empleado
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Employee Name")]
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+        public DateTime? RecordCreatedOn { get; set; }
+
+    }
+}
