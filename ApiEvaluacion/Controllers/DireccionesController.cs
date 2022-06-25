3a43e676dacf40edf457d43153c1136539135af2
watl <walter.martinez04@gmail.com>
2022-06-24 21:55:17 -0600

    controladores pendientes de subir

---
+++ ApiEvaluacion/Controllers/DireccionesController.cs
@@ -0,0 +1,28 @@
+﻿using ApiEvaluacion.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEvaluacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DireccionesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DireccionesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet(Name = "GetDireccionesList")]
+        public async Task<IActionResult> GetAsync()
+        {
+            var pnd = await _context.Direcciones.ToListAsync();
+            return Ok(pnd);
+        }
+
+
+    }
+}
