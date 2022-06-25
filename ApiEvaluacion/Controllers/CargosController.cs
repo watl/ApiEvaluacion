6662e9ebbde474883bd981aaa87a717866649d9f
CF0068\Walter Bermudez <walter.martinez04@gmail.com>
2022-06-24 21:39:12 -0600

    creacion de controladores para catalogos del front

---
+++ ApiEvaluacion/Controllers/CargosController.cs
@@ -0,0 +1,28 @@
+﻿using ApiEvaluacion.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEvaluacion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CargosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet(Name = "GetCargosList")]
+        public async Task<IActionResult> GetAsync()
+        {
+            var pnd = await _context.Cargos.ToListAsync();
+            return Ok(pnd);
+        }
+
+
+    }
+}
